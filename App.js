a2a52d2af9137997e6e868fd52f39c6cd99ca0f7
ManojSHub <kmanojat@gmail.com>
2025-07-28 21:52:38 +0530

    episode02

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement(
     "div",
-    {id: "parent"},
-    [React.createElement(
-        "div", 
-        {id:"child"},
-        [React.createElement("h1", {}, "I am H1 tag!"), React.createElement("h2", {}, "I am H2 tag!")] 
-    ),
-    React.createElement(
-        "div", 
-        {id:"child2"},
-        [React.createElement("h1", {}, "I am H1 tag!"), React.createElement("h2", {}, "I am H2 tag!")] 
-    )
+    { id: "parent" },
+    [
+        React.createElement(
+            "div",
+            { id: "child", key: "1" },
+            [
+                React.createElement("h1", { key: "1.1" }, "I am H1 tag!"),
+                React.createElement("h2", { key: "1.2" }, "I am H2 tag!")
+            ]
+        ),
+        React.createElement(
+            "div",
+            { id: "child2", key: "2" },
+            [
+                React.createElement("h1", { key: "2.1" }, "I am H1 tag!"),
+                React.createElement("h2", { key: "2.2" }, "I am H2 tag!")
+            ]
+        )
     ]
-)
+);
 
-console.log(parent);
+
+// console.log(parent);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
